3bf1b69418121d169d29a76faf6ee2d01bbfddff
Andrew Dabrowski <unhandyandy@gmail.com>
2024-09-17 22:59:00 -0400

    knightmorphs pieces

--- games/arithmachy/engine.js
+++ games/arithmachy/engine.js
@@ -341,7 +341,8 @@ function numberToSymbol( n ){
     case  4: return "\u25A1";
 	//    case 4: return "<img src=http://www.fileformat.info/info/unicode/char/25a2/white_square_with_rounded_corners.png style='height:0.8em;' class=char />";
 //    case  5: return "\u2605";
-    case  5: return "H";
+    // case  5: return "H";
+    case  5: return "\u2059";
 //    case  5: return "ӿ";
 //    case  5: return "\u2B53";
     case  6: return "\u2721";
@@ -357,15 +358,22 @@ function numberToSymbol( n ){
 //    case 11: return "\u296e";
 //    case 12: return "\uD83D\uDD55";
 //    case 12: return "\u27F3";
-    case 12: return "\u2739";
-//    case 13: return "\uD835\uDD10";
+    // case 12: return "\u2739";
+    // case 12: return "\u2725";
+    case 12: return "\u2743";
+//    case 12: return "IXI";
+// -
+	    //    case 13: return "\uD835\uDD10";
 	//    case 13: return "𝖃";
 //    case 13: return "\uD835\uDD83";
     case 13: return "}{";
 //    case 13: return "Ж";
-    case 14: return "\u2720";
-    case 15: return "\u272D";
-    case 16: return "\u2638";
+    // case 14: return "\u2720";
+    case 14: return "ZZ";
+    // case 15: return "\u272D";
+    case 15: return "O";
+    // case 16: return "\u2638";
+    case 16: return "\u273A";
     default: return n; }
 }
 
